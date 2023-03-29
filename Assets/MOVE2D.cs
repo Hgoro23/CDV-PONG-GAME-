edaa5d7f50104743730c3549d22f737953ce6906
hgoro <hgoro@Cedu.cdv.pl>
2023-03-29 16:10:25 +0200

    we created a two player game and added movent and key and position and validation

---
+++ Assets/MOVE2D.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVE2D : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody2D;
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(UpKey)&& transform.position.y<5)
+        {
+            rigidbody2D.velocity = Vector2.up * speed; }
+        else if (Input.GetKey(DownKey) && transform.position.y > -5)
+        {
+            rigidbody2D.velocity = Vector2.down * speed;
+        }
+        
+        else
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+              
+    }
+}
